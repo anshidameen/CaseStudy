10d49b38a1077abaaf0ff4290df33245dd83ea71
anshidameen <anshidameen@gmail.com>
2025-02-13 02:57:48 +0400

    endpoints for listItem and component to list item

--- src/app/feature/list-item/list-item.component.ts
+++ src/app/feature/list-item/list-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MockServiceService } from 'src/app/core/services/mock-service.service';
 
 @Component({
   selector: 'app-list-item',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListItemComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private mock:MockServiceService) { }
+  items:any[] =[];
   ngOnInit(): void {
+    this.getAllList();
   }
 
+  getAllList(){
+    this.mock.getList().subscribe((res)=>{
+      this.items=res
+    },
+    (error)=>{
+      console.error('Error fetching items', error);
+    })
+  }
 }
